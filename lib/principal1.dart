94783364f88fe09bbd1fe0e5ef47cc268c61e582
jpmoraes06 <150357838+jpmoraes06@users.noreply.github.com>
2024-04-04 12:18:47 -0300

    somativa

---
+++ lib/principal1.dart
@@ -0,0 +1,89 @@
+import 'principal1.dart';
+import 'imc2.dart';
+import 'contador3.dart';
+import 'cadastroproduto5.dart';
+import 'cadastrousuario4.dart';
+import 'package:flutter/material.dart';
+
+
+
+
+class Principal1 extends StatelessWidget {
+  const Principal1({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("MENU"),
+        centerTitle: true,
+        backgroundColor: Colors.deepOrangeAccent,
+      ),
+
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            const Icon(
+              Icons.add_business_sharp,
+              size: 120.0,
+              color: Colors.deepOrangeAccent,
+            ),
+            Text(
+              "Aplicação Principal",
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.blueGrey, fontSize: 25.0),
+            ),
+            Text(
+              "",
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.blueGrey, fontSize: 25.0),
+            ),
+            Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  ElevatedButton(
+                      child: const Text("IMC"),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const imc2()),
+                        );
+                      }),
+                  ElevatedButton(
+                      child: const Text("Contador"),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const contador3()),
+                        );
+                      }),
+                  ElevatedButton(
+                      child: const Text("Cadastro Usuario"),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const cadastrousuario4()),
+                        );
+                      }),
+                  ElevatedButton(
+                      child: const Text("Cadastro de produtos"),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const cadastroproduto5()),
+                        );
+                      }),
+                ]),
+          ],
+        ),
+      ), // Botão para executar o calculo do IMC,
+    );
+  }
+}
